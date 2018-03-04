6b5d9ef9a4f05548affb15856efb6ae98b9daecb
sh2435 <sh2435@cornell.edu>
2018-03-03 19:20:15 -0500

    delete test & rm alternative solution

--- a3/src/manip/RotationManipulator.java
+++ a3/src/manip/RotationManipulator.java
@@ -48,7 +48,7 @@ public class RotationManipulator extends Manipulator {
 		// Note that the mouse positions are given in coordinates that are normalized to the range [-1, 1]
 		//   for both X and Y. That is, the origin is the center of the screen, (-1,-1) is the bottom left
 		//   corner of the screen, and (1, 1) is the top right corner of the screen.
-		System.out.println("---------------------");
+//		System.out.println("---------------------");
 		Matrix4 viewinv = viewProjection.clone().invert();
 		Vector3 l = viewinv.mulPos(new Vector3(lastMousePos.x,lastMousePos.y,1f));
 		Vector3 l0 = viewinv.mulPos(new Vector3(lastMousePos.x,lastMousePos.y,-1f));
@@ -56,69 +56,33 @@ public class RotationManipulator extends Manipulator {
 		Vector3 c = viewinv.mulPos(new Vector3(curMousePos.x,curMousePos.y,1f));
 		Vector3 c0 = viewinv.mulPos(new Vector3(curMousePos.x,curMousePos.y,-1f));
 		c.sub(c0);
-//		Vector3 camd = viewinv.mulPos(new Vector3(0f,0f,1f));
-//		Vector3 cam = viewinv.mulPos(new Vector3(0f,0f,-1f));
-//		camd.sub(cam).normalize();
 		Vector3 or = this.reference.translation.mulPos(new Vector3(0f,0f,0f));
 		Vector3 bx = new Vector3(1f,0f,0f);
 		Vector3 by = new Vector3(0f,1f,0f);
 		Vector3 bz = new Vector3(0f,0f,1f);
-//		Matrix4 ro = this.reference.rotationX.clone();
-//		Vector3 bx = ro.mulPos(new Vector3(1f,0f,0f));
-//		bx.sub(or);
-//		Vector3 by = ro.mulPos(new Vector3(0f,1f,0f));
-//		by.sub(or);
-//		Vector3 bz = ro.mulPos(new Vector3(0f,0f,1f)).normalize();
-//		bz.sub(or);
 		switch(this.axis) {
 		case X:{
 			
 			Vector3 tl = help(by,bz,l,or,l0);
 			Vector3 tc = help(by,bz,c,or,c0);
-			Vector2 tll= new Vector2(tl.x,tl.y);
-			Vector2 tcc= new Vector2(tc.x,tc.y);
-//			float ang = tll.angle(tcc);
-//			System.out.println(or);
-//			System.out.println(tll);
 			Vector3 rl = l0.clone().add(l.mul(tl.z));//by.clone().mul(tl.x).add(bz.clone().mul(tl.y)).add(or);
 			Vector3 rc = c0.clone().add(c.mul(tc.z));//by.clone().mul(tc.x).add(bz.clone().mul(tc.y));
 			rl.sub(or);
 			rc.sub(or);
-//			System.out.println(rl);
-//			System.out.println(l0.clone().add(l.mul(tl.z)));
-//			System.out.println(tll);
-//			System.out.println(rc);
-//			System.out.println(tcc);
-//			System.out.println(rl.clone().cross(rc).normalize());
 			double ang = rl.clone().cross(rc).dot(bx)/rl.len()/rc.len();
 			ang = Math.asin(ang); 
 			if(Double.isNaN(ang))
 				return;
-//			double clock = tl.x*tc.y - tl.y*tc.x;
-//			if(clock<0)
-//				ang *= -1;
-//			System.out.println(ang);
-//			System.out.println(tll);
-//			System.out.println(tcc);
-//			double angl = rl.angle(by);
-//			if(rl.cross(by).dot(bx)>0)
-//				angl *= -1;
 			this.reference.rotationX.mulBefore(Matrix4.createRotationX((float)ang));
-//			this.reference.rotationX.set(Matrix4.createRotationX((float)angl));
 			break;
 		}
 		case Y:{
-//			Vector3 bx = new Vector3(1f,0f,0f);
-//			Vector3 by = new Vector3(0f,1f,0f);
-//			Vector3 bz = new Vector3(0f,0f,1f);
 			Matrix4 tr = this.reference.rotationX.clone();
 			bx = tr.mulPos(bx);
 			by = tr.mulPos(by);
 			bz = tr.mulPos(bz);
 			Vector3 tl = help(bx,bz,l,or,l0);
 			Vector3 tc = help(bx,bz,c,or,c0);
-			Vector2 tll= new Vector2(tl.x,tl.y);
-			Vector2 tcc= new Vector2(tc.x,tc.y);
 
 			Vector3 rl = l0.clone().add(l.mul(tl.z));
 			Vector3 rc = c0.clone().add(c.mul(tc.z));
@@ -131,36 +95,15 @@ public class RotationManipulator extends Manipulator {
 			
 			this.reference.rotationY.mulBefore(Matrix4.createRotationY((float)ang));
 
-			
-//			Vector3 tl = help(bx,bz,l,or,l0);
-//			Vector3 tc = help(bx,bz,c,or,c0);
-//			Vector2 tll= new Vector2(tl.x,tl.y);
-//			Vector2 tcc= new Vector2(tc.x,tc.y);
-//			float ang = tll.angle(tcc);
-//			if(Double.isNaN(ang))
-//				return;
-//			double clock = tl.x*tc.y - tl.y*tc.x;
-//			if(clock<0)
-//				ang *= -1;
-//			System.out.println(ang);
-//			System.out.println(tll);
-//			System.out.println(tcc);
-//
-//			this.reference.rotationY.mulBefore(Matrix4.createRotationY(ang));
 			break;
 		}
 		case Z:{
-//			Vector3 bx = new Vector3(1f,0f,0f);
-//			Vector3 by = new Vector3(0f,1f,0f);
-//			Vector3 bz = new Vector3(0f,0f,1f);
 			Matrix4 tr = this.reference.rotationX.clone().mulBefore(this.reference.rotationY);
 			bx = tr.mulPos(bx);
 			by = tr.mulPos(by);
 			bz = tr.mulPos(bz);
 			Vector3 tl = help(bx,by,l,or,l0);
 			Vector3 tc = help(bx,by,c,or,c0);
-			Vector2 tll= new Vector2(tl.x,tl.y);
-			Vector2 tcc= new Vector2(tc.x,tc.y);
 
 			Vector3 rl = l0.clone().add(l.mul(tl.z));
 			Vector3 rc = c0.clone().add(c.mul(tc.z));
@@ -172,21 +115,6 @@ public class RotationManipulator extends Manipulator {
 				return;
 			
 			this.reference.rotationZ.mulBefore(Matrix4.createRotationZ((float)ang));
-//			Vector3 tl = help(bx,by,l,or,l0);
-//			Vector3 tc = help(bx,by,c,or,c0);
-//			Vector2 tll= new Vector2(tl.x,tl.y);
-//			Vector2 tcc= new Vector2(tc.x,tc.y);
-//			float ang = tll.angle(tcc);
-//			if(Double.isNaN(ang))
-//				return;
-//			double clock = tl.x*tc.y - tl.y*tc.x;
-//			if(clock<0)
-//				ang *= -1;
-//			System.out.println(ang);
-//			System.out.println(tll);
-//			System.out.println(tcc);
-//
-//			this.reference.rotationZ.mulBefore(Matrix4.createRotationZ(ang));
 			break;
 		}
 		}
@@ -212,8 +140,6 @@ public class RotationManipulator extends Manipulator {
 		double srr = sr.determinant();
 		Vector3 ans = new Vector3((float)(s11/sll),(float)(s22/sll),(float)(srr/sll)); 
 		
-//		Vector3d ansl= new Vector3d(b.clone().sub(a));
-//		ansl=sl.clone().invert().mul(ansl);
 		return new Vector3(ans);
 	}
 	
